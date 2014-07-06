486a1c4e5dd4831b7c4e646653d0847255d620e1
natalieparellano <natalie.p.arellano@gmail.com>
2014-07-06 17:21:49 -0400

    Added .uniq to @events in the EventController to avoid duplication in the view

--- app/event_controller.rb
+++ app/event_controller.rb
@@ -2,7 +2,7 @@ class EventController < Sinatra::Base
     
   get '/' do
     Event.more
-    @events = Event.all.sort_by { |event| event.date }
+    @events = Event.all.sort_by { |event| event.date }.uniq
     @groups = @events.collect { |event| event.group }.uniq
     erb :index
   end
