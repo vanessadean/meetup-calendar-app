84a11d938c9656a9fc0389b8991803f479b6d4e5
natalieparellano <natalie.p.arellano@gmail.com>
2014-07-06 17:20:33 -0400

    No changes, just added comments to make sure I understand what is going on

--- app/app.rb
+++ app/app.rb
@@ -11,9 +11,12 @@ class App < Sinatra::Base
   end
 
   def self.members
+    # get all members of Flatiron Presents
     members_uri ||= URI("https://api.meetup.com/2/members?group_id=14306982&key=4d414d6bb7e7f7fb442a717a207f") 
     # includes Flatiron Presents group id and api key
 
+    # for each member, create a new instance of Member
+    # and assign meetup_id to the primary identifier given by the meetup API
     members_hash = JSON.parse(Net::HTTP.get(members_uri))
     members_hash["results"].each { |result| Member.create(:meetup_id => result["id"]) }
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
   belongs_to  :group
 
+  # use Flatiron Presents members as a starting seed
+  # and get the other events that they are members of 
   def self.more
     Member.all.sample(3).each do |member|
       uri = URI("https://api.meetup.com/2/events?&sign=true&photo-host=public&member_id=#{member.meetup_id}&time=1404858600000,%201405463400000&page=20&key=4d414d6bb7e7f7fb442a717a207f")
@@ -9,9 +11,16 @@ class Event < ActiveRecord::Base
       my_hash = JSON.parse(Net::HTTP.get(uri))
 
       my_hash["results"].each do |result|
+        # create the event if not exists
         event = self.find_or_create_by(:name => result["name"], :date => result["time"], :url => result["event_url"]) 
+
+        # create the group if not exists, and assign it to the event
         event.group = Group.find_or_create_by(:name => result["group"]["name"], :urlname => result["group"]["urlname"])
+
+        # assign the group color -- assign color is defined on Group
         event.group.color ||= event.group.assign_color
+
+        # add the current member to the group if not already a member
         event.group.members << member unless event.group.members.include?(member)
         event.save
       end
