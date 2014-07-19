2398b39ac52b7d6c37c349c624f1e53e138cb005
Vanessa Dean <vanessa.dean@gmail.com>
2014-07-19 13:21:54 -0400

    Update postgres connector

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require 'net/http'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection('postgres://localhost/project')
+ActiveRecord::Base.establish_connection('postgres://qbgnzmttolseof:32rlZpsC_1CmAhgeqtWb-Unmp-@ec2-54-225-168-181.compute-1.amazonaws.com:5432/dch7lkfh9f4rio')
 
 require_all 'app'
 
