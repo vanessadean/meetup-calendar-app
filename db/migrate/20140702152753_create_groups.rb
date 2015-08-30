7674c9554d9bd93718940b1606d33a75c7c4c1da
Vanessa Dean <vanessa.dean@gmail.com>
2015-08-30 15:54:00 -0400

    Remove database

--- db/migrate/20140702152753_create_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateGroups < ActiveRecord::Migration
-  def change
-    create_table :groups do |t|
-      t.string  :name
-      t.string  :color
-      t.string  :urlname
-    end
-  end
-end
